8eead89eb0bd6da8334c5dc0aad8416b5aeb5351
rodrira@gmail.com <rhoderamirez@Rhodes-MacBook-Pro.local>
2025-08-29 22:22:05 -0600

    Elimino Archivos duplicados

--- Addlistnumbers.py
+++
@@ -1,7 +0,0 @@
-def added_numbers(numbers):
-    return sum(numbers)
-
-
-numbers = [4, 6, 2, 29]
-result = added_numbers(numbers)
-print( 'The sum of all numbers is:', result)
--- alphabethic.py
+++
@@ -1,7 +0,0 @@
-def sorted_words(text):
-    words = text.split('-')
-    words = sorted(words)
-    return '-'.join(words)
-
-result = sorted_words('python-variable-funcion-computadora-monitor')
-print(result)
