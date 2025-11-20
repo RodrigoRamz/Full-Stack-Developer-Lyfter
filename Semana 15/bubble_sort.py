705bd65c8e0c4323a2882f974d9933cb39d59264
rodrira@gmail.com <rhoderamirez@Rhodes-MacBook-Pro.local>
2025-11-20 16:49:52 -0600

    Semana 15 Bubble Sort Optimized

--- Semana 15/bubble_sort.py
+++ Semana 15/bubble_sort.py
@@ -1,11 +1,19 @@
 
-def bubble_sort_reverse(list): # step 1: How many elements are in the list
-    n = len(list)
+def bubble_sort_reverse(list): # step 1: How many elements are in the list # O(n2)
+    n = len(list) # O(1)
 
-    for lap in range(n - 1): # step 2: Repeat Laps
+    for lap in range(n - 1): # step 2: Repeat Laps # O(1)
 
-        for i in range(n - 1, 0, -1): # step 3: run from left to right
+        for i in range(n - 1, lap, -1): # step 3: run from left to right # O(n) #freeze & save comparisons left side is already sorted
 
-            if list[i] < list[i - 1]: # step 4: compare the actual element with the next one
-                list[i], list[i - 1] = list[i - 1], list[i] # step 5: interchange elements if the order is wrong
-                
+            if list[i] < list[i - 1]: # step 4: compare the actual element with the next one # O (1)
+                list[i], list[i - 1] = list[i - 1], list[i] # step 5: interchange elements if the order is wrong # O(1)
+                changes = True
+
+        if changes: #if there are no changes then the list is already sorted
+            break
+
+list = [9, 5, 7, 1, 3]
+print("Before:", list)
+bubble_sort_reverse(list)
+print("After:", list)
